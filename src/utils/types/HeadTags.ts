6efa6694cb92e242f86a8cd95969091eb23e6aec
ricardovac <ricardoviicius@gmail.com>
2023-10-04 03:03:38 -0300

    feat: change bg color and add sal.js

--- src/utils/types/HeadTags.ts
+++ src/utils/types/HeadTags.ts
@@ -1,12 +1,4 @@
 export type HeadTags = {
   title?: string;
   description?: string;
-  noindex?: boolean;
-  og?: {
-    title: string;
-    type: string;
-    description: string;
-    image: string;
-    alt: string;
-  };
 };
